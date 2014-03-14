cad5dd47278552eb9e5f5aa0e596cd92fa3d646c
Christian Weiske <christian.weiske@netresearch.de>
2014-03-14 11:26:14 +0100

    move data.json to single.json

--- example/run.php
+++ example/run.php
@@ -3,7 +3,7 @@ require_once __DIR__ . '/../src/JsonMapper.php';
 require_once 'Contact.php';
 require_once 'Address.php';
 
-$json = json_decode(file_get_contents(__DIR__ . '/data.json'));
+$json = json_decode(file_get_contents(__DIR__ . '/single.json'));
 
 $mapper = new JsonMapper();
 $contact = $mapper->map($json, new Contact());
