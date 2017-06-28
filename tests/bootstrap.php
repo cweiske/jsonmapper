f42ffd3b8d44c020d28aeb25ae5632feb930ce22
Thomas Hansen <thomas@spysystem.dk>
2017-06-28 09:21:14 +0200

    Adjustments to phpunit bootstrap, travis script, phpunit.xml and composer.json

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,9 +7,4 @@ if (is_dir(__DIR__ . '/../src/')) {
 }
 include __DIR__ . '/../vendor/autoload.php';
 
-// backward compatibility
-if (!class_exists('\PHPUnit\Framework\TestCase') &&
-    class_exists('\PHPUnit_Framework_TestCase')) {
-    class_alias('\PHPUnit_Framework_TestCase', 'PHPUnit\Framework\TestCase');
-}
 ?>
