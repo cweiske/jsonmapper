0461b09b64a3d577104eeb6fff08ef44c9375164
Reinfi <reinfandt@z-em.de>
2022-12-27 20:29:11 +0100

    add test case for enums

---
+++ tests/Enums/ObjectWithEnum.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Enums;
+
+class ObjectWithEnum
+{
+    public StringBackedEnum $stringBackedEnum;
+    public IntBackedEnum $intBackedEnum;
+    public SimpleEnum $simpleEnum;
+}
---
+++ tests/Enums/SimpleEnum.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Enums;
+
+enum SimpleEnum
+{
+    case foo;
+    case bar;
+}
