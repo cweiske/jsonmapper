cweiske/jsonmapper

commit 67632ea5e671052b87a7659550fa0374883b9d77

Fix broken "array"-typed properties
Resolves: https://github.com/cweiske/jsonmapper/issues/205
Close: https://github.com/cweiske/jsonmapper/pull/206

src/JsonMapper.php
@@ -242,7 +242,9 @@ class JsonMapper
             } else if ($this->isObjectOfSameType($type, $jvalue)) {
                 $this->setProperty($object, $accessor, $jvalue);
                 continue;
-            } else if ($this->isSimpleType($type) && !is_array($jvalue)) {
+            } else if ($this->isSimpleType($type)
+                && !(is_array($jvalue) && $this->hasVariadicArrayType($accessor))
+            ) {
                 if ($type === 'string' && is_object($jvalue)) {
                     throw new JsonMapper_Exception(
                         'JSON property "' . $key . '" in class "'

tests/Array_PHP74_Test.php
@@ -1,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Unit tests for JsonMapper's support for PHP 7.4 typed arrays
+ *
+ * @category Tools
+ * @package  JsonMapper
+ * @author   Martin Reinfandt <martin.reinfandt@check24.de>
+ * @license  OSL-3.0 http://opensource.org/licenses/osl-3.0
+ * @link     https://github.com/cweiske/jsonmapper
+ * @requires PHP 7.4
+ */
+class Array_PHP74_Test extends TestCase
+{
+    protected function setUp(): void
+    {
+        require_once 'JsonMapperTest/PHP74_Array.php';
+    }
+
+    public function testJsonMapper()
+    {
+        $json = json_decode('{"files": ["test.txt"]}');
+        $jsonMapper = new \JsonMapper();
+        $array = $jsonMapper->map($json, new PHP74_Array());
+        self::assertCount(1, $array->files);
+    }
+}

tests/JsonMapperTest/PHP74_Array.php
@@ -1,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+class PHP74_Array
+{
+    public array $files;
+}

commit 543f3af933f178bbf437d3f0a7d670a5596a1ed5

Added failing test case for array to string bug introduced by 67632ea5e671052b87a7659550fa0374883b9d77
The included test case should explain the problem.

I don't know what the correct fix for this is right now.

tests/JsonMapperTest/ArrayValueForStringProperty.php
@@ -1,0 +1,16 @@
+<?php
+/**
+ * Part of JsonMapper
+ *
+ * PHP version 5
+ *
+ * @package  JsonMapper
+ * @author   Christian Weiske <cweiske@cweiske.de>
+ * @license  OSL-3.0 http://opensource.org/licenses/osl-3.0
+ * @link     http://cweiske.de/
+ */
+
+class JsonMapperTest_ArrayValueForStringProperty
+{
+	public string $value;
+}

tests/SimpleTest.php
@@ -10,7 +10,9 @@
  * @license  OSL-3.0 http://opensource.org/licenses/osl-3.0
  * @link     https://github.com/cweiske/jsonmapper
  */
+
 require_once 'JsonMapperTest/Simple.php';
+require_once 'JsonMapperTest/ArrayValueForStringProperty.php';
 
 /**
  * Unit tests for JsonMapper's simple type handling
@@ -246,6 +248,16 @@ class SimpleTest extends \PHPUnit\Framework\TestCase
             'blubb', $sn->internalData['hyphen-value-setter']
         );
 
+    }
+
+    public function testMapArrayValueToStringProperty()
+    {
+        $jm = new JsonMapper();
+        $this->expectException(JsonMapper_Exception::class);
+        $jm->map(
+            json_decode('{"value":[]}'),
+            new JsonMapperTest_ArrayValueForStringProperty()
+        );
     }
 }
 ?>