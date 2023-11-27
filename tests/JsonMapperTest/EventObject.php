705f714fa136203f731570b882b1187ff3d7b9c0
Maik Schneider <mas@xima.de>
2023-11-27 09:12:24 +0100

    Add optional arguments to post mapping event
    Resolves: https://github.com/cweiske/jsonmapper/issues/217
    Resolves: https://github.com/cweiske/jsonmapper/pull/218

--- tests/JsonMapperTest/EventObject.php
+++ tests/JsonMapperTest/EventObject.php
@@ -10,5 +10,14 @@ class JsonMapperTest_EventObject
     {
         $this->pStr = 'two';
     }
+
+    /**
+     * @param int $arg1
+     * @param string $arg2
+     */
+    private function _deserializePostEventWithArguments($arg1, $arg2)
+    {
+        $this->pStr = str_repeat($arg2, $arg1);
+    }
 }
 ?>
